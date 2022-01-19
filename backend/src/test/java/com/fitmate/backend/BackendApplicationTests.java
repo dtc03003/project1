ba131e7530d885ff9e6aef213286a85c4421813d
이홍준 <zunza96@naver.com>
2022-01-19 18:14:00 +0900

    init

---
+++ backend/src/test/java/com/fitmate/backend/BackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fitmate.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
