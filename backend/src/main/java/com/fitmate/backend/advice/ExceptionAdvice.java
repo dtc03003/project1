90bfd04c8dd1b0f89c0eb9f72ac157ff8fd6180f
이홍준 <zunza96@naver.com>
2022-01-23 03:11:08 +0900

    FEAT: init adviceController branch

---
+++ backend/src/main/java/com/fitmate/backend/advice/ExceptionAdvice.java
@@ -0,0 +1,9 @@
+package com.fitmate.backend.advice;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RequiredArgsConstructor
+@RestControllerAdvice
+public class ExceptionAdvice {
+}
