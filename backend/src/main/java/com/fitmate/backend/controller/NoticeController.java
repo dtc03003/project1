02b623c46c82db921054473eb591eb3685840260
정종혁 <whdgur1068@naver.com>
2022-01-26 16:57:22 +0900

    FEAT: Signup

--- backend/src/main/java/com/fitmate/backend/controller/NoticeController.java
+++ backend/src/main/java/com/fitmate/backend/controller/NoticeController.java
@@ -14,6 +14,7 @@ import java.util.List;
 
 @Tag(name = "NoticeController", description = "Notion board 게시글 정보 처리")
 @RestController
+@CrossOrigin(origins = "*")
 @RequiredArgsConstructor
 @RequestMapping(value = "/api/v1")
 public class NoticeController {
