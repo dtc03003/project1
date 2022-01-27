f2ff29b75288a326bade9db8995effeae97609ec
서지원 <yogjesi@naver.com>
2022-01-27 14:59:19 +0900

    FEAT: 178 사진 띄우기 기타: 연동은 DB 연결하고 할 것

---
+++ frontend/fitmate/src/components/Stylist/TheImageModal.vue
@@ -0,0 +1,32 @@
+<template>
+	<span>
+    <img src="@/assets/study.jpg" @click="$bvModal.show('bv-modal-example')" height="150px" class="m-1">
+    <b-modal size="lg" id="bv-modal-example" hide-footer>
+      <template #modal-title>
+        <b-avatar src="https://placekitten.com/300/300" size="4rem">
+        </b-avatar>
+      </template>
+      <div class="d-inline">
+        <!-- 상세 이미지 -->
+        <img src="@/assets/study.jpg" alt="" height="400rem" class="mr-2">
+        <!-- 태그 -->
+      </div>
+      <b-button class="mt-3 d-block" block @click="$bvModal.hide('bv-modal-example')">Close Me</b-button>
+    </b-modal>
+    </span>
+</template>
+
+<script>
+export default {
+  name: 'TheStylistListImage',
+  components: {},
+  data: function() {
+
+  },
+
+}
+</script>
+
+<style>
+
+</style>
