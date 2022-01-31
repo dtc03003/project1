32dc99d0fc14180d7440215652c99af0f838bb36
박현진 <jinnydiary@naver.com>
2022-01-31 15:49:46 +0900

    feat: 스케줄 구현 - 지나간 날짜, 지정 날짜, 예약된 시간 비활성화 및 선택 불가능한 시간 alert 설정

--- frontend/fitmate/src/components/portfolio/schedule/ScheduleMember.vue
+++ frontend/fitmate/src/components/portfolio/schedule/ScheduleMember.vue
@@ -16,18 +16,21 @@
                 <b-col cols="1"></b-col>
                 <b-col>
                     <h4>시간선택</h4>
+                    <h6>(시간은 기본 2시간 단위입니다.)</h6>
                     <v-item-group class="mt-3">
                         <v-row>
                             <table>
                                 <tr v-for="(time, idx) in times" :key="time[idx]">
                                     <td v-for="(t) in time" :key="t">
-                                        <div class="align-center btn mb-2" ref="selectedTime" @click="selectTime(t)">{{ t }}</div>
+                                        <div ref="selectedTime" @click="selectTime(t)"
+                                        :class="reservedTime.includes(t) ? 'align-center btn mb-2 disabled' : 'align-center btn mb-2'"
+                                        >{{ t }}</div>
+                                        <!-- v-bind:disabled="reservedTime.includes(t) ? true : false" -->
                                     </td>
                                 </tr>
                             </table>
                         </v-row>
                     </v-item-group>
-                    <v-btn class="mt-5" align="right" v-show="selectedTime" @click="moveOrder">결제하기</v-btn>
                 </b-col>
             </b-row>
         </b-container>
@@ -42,8 +45,8 @@ export default {
     name: "ScheduleMember",
     data() {
         return {
-            picker: (new Date(Date.now() - (new Date()).getTimezoneOffset() * 60000)).toISOString().substr(0, 10), //현재 날짜
-            // picker: '',
+            // picker: (new Date(Date.now() - (new Date()).getTimezoneOffset() * 60000)).toISOString().substr(0, 10), //현재 날짜
+            picker: '',
             times: [
                 ["10:00", "10:30", "11:00", "11:30"],
                 ["12:00", "12:30", "13:00", "13:30"],
@@ -51,13 +54,23 @@ export default {
                 ["16:00", "16:30", "17:00", "17:30"],
                 ["18:00", "18:30", "19:00", "19:30"]
             ],
+            calcTimes: [
+                "10:00", "10:30", "11:00", "11:30",
+                "12:00", "12:30", "13:00", "13:30",
+                "14:00", "14:30", "15:00", "15:30",
+                "16:00", "16:30", "17:00", "17:30",
+                "18:00", "18:30", "19:00", "19:30"
+            ],
             selectedTime: '',
+            reservedTime: ["10:00", "15:00"], //DB에서 받아올 값(스타일리스트, 날짜 값을 통해 이미 예약된 시간 얻어오기)
         }
+    },
+    created() {
+        this.importTime();
     },
     watch: {
         picker: function() {
-            this.SET_DATE(this.picker);
-            this.selectedTime = '';
+            this.importTime();
         },
         selectedTime: function() {
             this.SET_TIME(this.selectedTime);
@@ -70,7 +83,10 @@ export default {
         allowedDates(value) {
             const date = dayjs(value);
             const now = dayjs((new Date(Date.now() - (new Date()).getTimezoneOffset() * 60000)).toISOString().substr(0, 10));
-            //주말 및 과거 날짜 제외한 나머지 날짜 활성화(이후 예약 날짜에 시간 다 찼을 시 해당 날짜도 비활성화)
+
+            //과거 날짜 제외한 나머지 날짜 활성화(스타일리스트가 임의로 불가한 요일(해당 날짜)도 이후 비활성화)
+            const impossible = [1, 5]; //임의로 설정한 불가능한 날짜(월요일, 금요일)
+            
             //생각해보니 주말에도 이용자가 있을 수도! 혹여 주말까지 비활성화 하려면 weekday !== 0 && weekday !== 6 도 적기!!
             // const weekday = date.get("d");
             // return weekday !== 0 && weekday !== 6 &&
@@ -78,40 +94,47 @@ export default {
                 (now.get("y") < date.get("y")) || 
                 ((now.get("y") == date.get("y")) && (now.get("M") < date.get("M"))) ||
                 ((now.get("y") == date.get("y")) && (now.get("M") == date.get("M")) && (now.get("D") <= date.get("D"))) )
+                && (!impossible.includes(date.get("d")))
         },
         selectTime(value) {
-            this.selectedTime = value;
+            let result = this.calcTimes[this.calcTimes.indexOf(value)+3];
+            if(!this.reservedTime.includes(result)) {
+                this.selectedTime = value;
+                this.moveOrder();
+            }else {
+                alert("예약 불가능한 시간입니다. 다른 시간을 골라주세요(기본 단위:2시간)");
+                this.selectedTime = '';
+            }
         },
         moveOrder() {
             if(confirm(`날짜: ${this.picker}\n시간: ${this.selectedTime}\n예약을 진행할까요?`))
             this.$router.push({name: "Order"});
         },
+        importTime() { //해당 날짜의 불가/예약된 시간 가져오기 - db 연동 필요
+            this.SET_DATE(this.picker);
+            this.selectedTime = '';
+            
+            this.reservedTime = ["10:00", "15:00"]; //- db 연동해서 저장될 예약 시간(임시)
 
+            let temp = [];
+            for(let time of this.reservedTime) {
+                let index = this.calcTimes.indexOf(time);
+                for(let n = index; n < index+4; ++n) {
+                    temp.push(this.calcTimes[n])
+                }
+            }
+            this.reservedTime = temp;
+        }
         // dateClass(ymd, date) {
         //     const day = date.getDate()
         //     return day >= 10 && day <= 20 ? 'table-info' : ''
-        // },
-        // dateDisabled(ymd, date) {
-            //주말은 선택 비활(일요일: 0, 토요일: 6)
-            //평일은 스타일리스트에 따라 안되는 날짜는 비활성화
-            // const weekday = date.getDay()
-            // const day = date.getDate()
-            //true면 비활성화
-            // return weekday === 0 || weekday === 6
         // },
     }
 }
 </script>
 
 <style scoped>
-#cardcss {background-color: #76e488; font-size: 20%; max-width: 70%; }
-.cardgroup {min-width: 40%;}
 .container {margin: 0 auto;}
 .btn {background-color: #d8d7ec; width: 80%; font-size: 10pt; border-radius: 0} /*#E0FFFF #F0F8FF*/
-.active {background-color: red;}
-/* button:focus, button:active {
-    box-shadow: none;
-    border: none;
-    outline: none;
-} */
+.disabled { background: rgb(184, 181, 181); }
 </style>
