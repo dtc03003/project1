a89fbf4b5b1e4d6a0d949b232192d3068df04bd8
박현진 <jinnydiary@naver.com>
2022-02-03 00:13:09 +0900

    feat: 스케줄(스타일리스트) 구현 - 스케줄(스타일리스트) 틀 제작중

--- frontend/fitmate/src/components/portfolio/schedule/ScheduleStyle.vue
+++ frontend/fitmate/src/components/portfolio/schedule/ScheduleStyle.vue
@@ -1,15 +1,217 @@
 <template>
     <div>
-        스타일리스트
+        <v-app>
+            <v-row class="fill-height">
+                <v-col>
+                    <!--상위(달/월/일 선택 및 스케줄 등록)-->
+                    <v-sheet height="64">
+                        <v-toolbar flat>
+                            <!--오늘 날짜로 이동-->
+                            <v-btn outlined class="mr-4" color="grey darken-2" @click="setToday">
+                                Today
+                            </v-btn>
+                            
+                            <!--이전 달/주/일로 이동-->
+                            <v-btn fab text small color="grey darken-2" @click="prev">
+                                <v-icon> mdi-chevron-left </v-icon>
+                            </v-btn>
+
+                            <!--현재 년/월 표시-->
+                            <v-toolbar-title v-if="$refs.calendar">
+                                {{ $refs.calendar.title }}
+                            </v-toolbar-title>
+                            
+                            <!--다음 달/주/일로 이동-->
+                            <v-btn fab text small color="grey darken-2" @click="next">
+                                <v-icon> mdi-chevron-right </v-icon>
+                            </v-btn>
+                            
+                            <v-spacer></v-spacer> <!--오른쪽 정렬-->
+
+                            <!--일정 등록-->
+                            <v-btn fab text small color="grey darken-2" id="enrollSchedule" @click="showEnroll">
+                                <v-icon id="enrollIcon"> mdi-plus </v-icon>
+                            </v-btn>
+
+                            <sch-event v-show="enroll == true" :enroll="this.enroll"/>
+
+                            <!--달/주/일 선택 리스트-->
+                            <v-menu bottom right>
+                                <template v-slot:activator="{ on, attrs }">
+                                    <v-btn outlined color="grey darken-2" v-bind="attrs" v-on="on">
+                                        <span>{{ typeToLabel[type] }}</span> <!--월/주/일 중 어떤 것 선택했는지 표시-->
+                                        <v-icon right>mdi-menu-down</v-icon>
+                                    </v-btn>
+                                </template>
+                                <v-list>
+                                    <v-list-item @click="type = 'day'">
+                                        <v-list-item-title>Day</v-list-item-title>
+                                    </v-list-item>
+                                    <v-list-item @click="type = 'week'">
+                                        <v-list-item-title>Week</v-list-item-title>
+                                    </v-list-item>
+                                    <v-list-item @click="type = 'month'">
+                                        <v-list-item-title>Month</v-list-item-title>
+                                    </v-list-item>
+                                </v-list>
+                            </v-menu>
+                        </v-toolbar>
+                    </v-sheet>
+                    
+                    <!--하위(스케줄표)(여기부터 수정하면 될듯)-->
+                    <v-sheet height="600">
+                        <!--오늘 날짜 클릭시 오늘 스케줄표로 이동(day)-->
+                        <v-calendar ref="calendar" v-model="focus" color="#bbbbe0" :events="events" :event-color="getEventColor"
+                        :type="type" @click:event="showEvent" @click:more="viewDay" @click:date="viewDay" @change="updateRange">
+                        </v-calendar>
+                        
+                        <!--
+                            close-on-content-click: 메뉴의 컨텐츠를 클릭하면 메뉴를 닫음
+                        -->
+                        <!--상세보기 창(dialog)-->
+                        <v-dialog v-model="selectedOpen" persistent max-width="600px" flat
+                        :close-on-content-click="false" :activator="selectedElement" offset-x>
+                        <v-card min-width="350px" flat>
+                            <v-card-title>
+                                <span class="text-h5">일정</span>
+                            </v-card-title>
+                            <v-card-text>
+                                <!--
+                                    필요한 내용: 예약자명(닉네임), 예약자이메일, 날짜, 시간
+                                -->
+                                <v-container>
+                                    <v-row>
+                                        <v-col cols="12">
+                                            <v-text-field label="예약자" prepend-icon="mdi-account"></v-text-field>
+                                            <v-text-field label="예약자이메일" prepend-icon="mdi-email"></v-text-field>
+                                            <v-text-field label="일정내용" prepend-icon="mdi-pencil"></v-text-field>
+                                        </v-col>
+                                        <v-col cols="12">
+                                            <v-text-field label="일정날짜" prepend-icon="mdi-calendar"></v-text-field>
+                                            <v-text-field label="시작시간" prepend-icon="mdi-clock"></v-text-field>
+                                            <v-text-field label="종료시간" prepend-icon="mdi-clock"></v-text-field>
+                                        </v-col>
+                                    </v-row>
+                                </v-container>
+                            </v-card-text>
+                            <v-card-actions>
+                                <v-spacer></v-spacer>
+                                <v-btn color="blue darken-1" text @click="selectedOpen = false">
+                                    Close
+                                </v-btn>
+                                <v-btn color="blue darken-1" text @click="selectedOpen = false">
+                                    Save
+                                </v-btn>
+                            </v-card-actions>
+                        </v-card>
+                        </v-dialog>
+
+                    </v-sheet>
+                </v-col>
+            </v-row>
+        </v-app>
     </div>
 </template>
 
 <script>
+import SchEvent from './SchEvent.vue'
 export default {
+  components: { SchEvent },
     name: "ScheduleStyle",
+    data() {
+        return {
+            focus: '', //현재 선택한 날짜
+            type: 'month', //초기 스케줄표 타입
+            typeToLabel: { //리스트
+                month: 'Month',
+                week: 'Week',
+                day: 'Day',
+            },
+            selectedEvent: {},
+            selectedElement: null,
+            selectedOpen: false,
+            events: [], //스케줄표에 작성된 일정들
+            colors: ['#F08080', '#98FB98', '#DDA0DD', '#7B68EE', '#EEE8AA'], //색상들
+            names: ['Meeting', 'Holiday', 'PTO', 'Travel', 'Event', 'Birthday', 'Conference', 'Party'],
+            enroll: false,
+        }
+    },
+    mounted() {
+        this.$refs.calendar.checkChange()
+    },
+    methods: {
+        viewDay({ date }) {
+            this.focus = date
+            this.type = 'day'
+        },
+        getEventColor(event) { //하루 일정표에서 확인 가능
+            return event.color
+        },
+        setToday() {
+            this.focus = ''
+        },
+        prev() {
+            this.$refs.calendar.prev()
+        },
+        next() {
+            this.$refs.calendar.next()
+        },
+        showEnroll() {
+            this.enroll = true;
+        },
+        //-------아래부터 수정해야 함-------
+        showEvent({ nativeEvent, event }) {
+            const open = () => {
+                this.selectedEvent = event
+                this.selectedElement = nativeEvent.target
+                requestAnimationFrame(() => requestAnimationFrame(() => this.selectedOpen = true))
+                //window.requestAnimationFrame()은 브라우저에게 수행하기를 원하는 애니메이션을 알리고
+                //다음 리페인트가 진행되기 전에 해당 애니메이션을 업데이트하는 함수를 호출하게 합니다. 이 메소드는 리페인트 이전에 실행할 콜백을 인자로 받습니다.
+            }
+
+            if(this.selectedOpen) {
+                this.selectedOpen = false
+                requestAnimationFrame(() => requestAnimationFrame(() => open()))
+            }else {
+                open()
+            }
+            
+            nativeEvent.stopPropagation()
+        },
+        updateRange ({ start, end }) {
+            const events = []
+            
+            const min = new Date(`${start.date}T00:00:00`)
+            const max = new Date(`${end.date}T23:59:59`)
+            const days = (max.getTime() - min.getTime()) / 86400000
+            const eventCount = this.rnd(days, days + 20)
+
+        for (let i = 0; i < eventCount; i++) {
+          const allDay = this.rnd(0, 3) === 0
+          const firstTimestamp = this.rnd(min.getTime(), max.getTime())
+          const first = new Date(firstTimestamp - (firstTimestamp % 900000))
+          const secondTimestamp = this.rnd(2, allDay ? 288 : 8) * 900000
+          const second = new Date(first.getTime() + secondTimestamp)
+
+          events.push({
+            name: this.names[this.rnd(0, this.names.length - 1)],
+            start: first,
+            end: second,
+            color: this.colors[this.rnd(0, this.colors.length - 1)],
+            timed: !allDay,
+          })
+        }
+
+        this.events = events
+      },
+      rnd (a, b) {
+        return Math.floor((b - a + 1) * Math.random()) + a
+      },
+    },
 }
 </script>
 
 <style scoped>
-
+#enrollSchedule {background-color: #6495ED; margin-right: 1%;}
+#enrollIcon {color: white;}
 </style>
