f862586c955f218d7300a423408279eca5969a61
seojiwon0702 <seojiwon335@gmail.com>
2022-02-03 10:47:09 +0900

    FIX:Get Comments

--- backend/src/main/java/com/fitmate/backend/repository/CommentRepository.java
+++ backend/src/main/java/com/fitmate/backend/repository/CommentRepository.java
@@ -4,8 +4,10 @@ import com.fitmate.backend.entity.Comment;
 import com.fitmate.backend.entity.Qna;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
 import java.util.Optional;
 
 public interface CommentRepository extends JpaRepository<Comment, Long> {
     Optional<Comment> findByQna(Qna qna);
+    Optional<List<Comment>> findAllByQna(Qna qna);
 }
