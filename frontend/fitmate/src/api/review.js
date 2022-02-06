5828197f29f47a6131702225f318fe4ca1b48808
박현진 <jinnydiary@naver.com>
2022-02-06 13:05:16 +0900

    feat: 포폴 존재 여부 확인 api 추가 및 리뷰 관련 상태 관리(vuex) 생성

--- frontend/fitmate/src/api/review.js
+++ frontend/fitmate/src/api/review.js
@@ -2,6 +2,11 @@ import { apiInstance } from "./index.js";
 
 const api = apiInstance();
 
+//포트폴리오 존재 여부 확인
+async function findPortfolio(nickname, success, fail) {
+    await api.get(`/api/v1/portfolio/${nickname}`).then(success).catch(fail);
+}
+
 //특정 스타일리스트의 리뷰 쓰기
 async function writeReview(review, nickname, success, fail) {
     await api.post(`/api/v1/portfolio/${nickname}/review`, JSON.stringify(review)).then(success).catch(fail);
@@ -22,4 +27,4 @@ async function reviewOne(nickname, id, success, fail) {
     await api.get(`/api/v1/portfolio/${nickname}/review/${id}`).then(success).catch(fail);
 }
 
-export { writeReview, reviewByPage, reviewAll, reviewOne };
+export { writeReview, reviewByPage, reviewAll, reviewOne, findPortfolio };
