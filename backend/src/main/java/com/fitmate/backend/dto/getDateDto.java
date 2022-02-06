f7ffb28cdc1594f46316deafdb95b066e3fcc511
이홍준 <zunza96@naver.com>
2022-02-07 01:58:49 +0900

    FEAT: create ReservationDto

---
+++ backend/src/main/java/com/fitmate/backend/dto/getDateDto.java
@@ -0,0 +1,22 @@
+//package com.fitmate.backend.dto;
+//
+//import lombok.Data;
+//import lombok.NoArgsConstructor;
+//
+//import javax.validation.constraints.Max;
+//import javax.validation.constraints.Min;
+//import javax.validation.constraints.NotBlank;
+//
+//@NoArgsConstructor
+//@Data
+//public class getDateDto {
+//    @Min(value = 1, message = "월이 1보다 작습니다.")
+//    @Max(value = 12, message = "월이 12보다 큽니다.")
+//    private int month;
+//
+//    @Min(value = 2022, message = "연도가 2022보다 작습니다.")
+//    private int year;
+//
+//    @NotBlank(message = "자리가 빈칸입니다.")
+//    private String seat;
+//}
