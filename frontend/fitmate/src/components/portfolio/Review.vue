f1cfc896168fe3c7d2a711db561951a375690cc8
박현진 <jinnydiary@naver.com>
2022-02-07 15:26:09 +0900

    feat:페이징 처리(page 문제는 이후 해결)

--- frontend/fitmate/src/components/portfolio/Review.vue
+++ frontend/fitmate/src/components/portfolio/Review.vue
@@ -1,37 +1,70 @@
 <template>
     <div>
-        <ReviewDetail v-for="(review, idx) in reviews" :key="idx" :idx="idx"/>
+        <ReviewDetail id="my-review" v-for="(review, idx) in someReviews" :key="review.id" :idx="idx"/>
+        <b-pagination align="center" class="mt-2"
+            v-model="page"
+            :total-rows="rows"
+            :per-page="perPage"
+            @change="changePage"
+        ></b-pagination>
     </div>
 </template>
 
 <script>
-import { mapActions, mapGetters } from 'vuex'
+import { mapActions, mapGetters, mapMutations } from 'vuex'
 import ReviewDetail from './review/ReviewDetail.vue';
 const reviewStore = "reviewStore";
 
 export default {
-  components: { ReviewDetail },
     name: 'Review',
     data() {
         return {
-            reviews: {},
+            reviews: [],
+            allReviews: [],
+            someReviews: [],
+            page: 1,
+            perPage: 5,
+            nickname: '',
         }
     },
+    components: { ReviewDetail },
     created() {
-        this.importReviews("지니쓰");
+        this.nickname = '지니쓰'; //스타일리스트 이름 받아오게되면 변경
+        this.importReviews(this.nickname);
     },
     computed: {
-        ...mapGetters(reviewStore, ["getReviews", "getPortfolioStatus"]),
+        ...mapGetters(reviewStore, ["getReviews", "getPortfolioStatus", "getSomeReviews"]),
+        rows() {
+        return this.allReviews.length
+        },
     },
     methods: {
-        ...mapActions(reviewStore, ["importAllReviews", "findPortfolioStatus"]),
+        ...mapMutations(reviewStore, ["SET_SOME_REVEIW_LIST", "SET_REVEIW_LIST"]),
+        ...mapActions(reviewStore, ["importAllReviews", "findPortfolioStatus", "fineReviewsByPage"]),
         async importReviews(nickname) {
-            await this.findPortfolioStatus(nickname);
+            await this.findPortfolioStatus(nickname); //해당 스타일리스트 모든 리뷰 불러오기
+            const info = {
+                nickname: nickname,
+                page: this.page //처음에는 1페이지
+            }
+            await this.fineReviewsByPage(info); //해당 스타일리스트 리뷰 5개씩 불러오기
             if(this.getPortfolioStatus) {
                 await this.importAllReviews(nickname);
+                this.allReviews = this.getReviews;
                 this.reviews = this.getReviews;
-                console.log(this.reviews)
+                this.SET_REVEIW_LIST(this.reviews);
+                this.someReviews = this.getSomeReviews; //페이지별 가져오기
             }
+        },
+        async changePage() {
+            console.log(this.page)
+            const info = {
+                nickname: this.nickname,
+                page: this.page
+            }
+            this.SET_SOME_REVEIW_LIST([]);
+            await this.fineReviewsByPage(info);
+            this.someReviews = this.getSomeReviews;
         }
     }
 
