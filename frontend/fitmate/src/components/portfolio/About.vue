bbabb45903ee2e66e039c657b4c029c758cd8377
김동현 <dtc03003@naver.com>
2022-02-07 15:20:34 +0900

    FEAT: portfolio profile, about 구현

--- frontend/fitmate/src/components/portfolio/About.vue
+++ frontend/fitmate/src/components/portfolio/About.vue
@@ -1,5 +1,34 @@
 <template>
     <div>
         <h1>About </h1>
+        <p>{{profileData.about}}</p>
     </div>
 </template>
+
+<script>
+import axios from "@/module/axios.js";
+import {mapState} from 'vuex'
+
+const memberStore = "memberStore";
+
+export default {
+    data() {
+        return {
+            profileData : [],
+        }
+    },
+
+    computed: {
+        ...mapState(memberStore, ["memberInfo"]),
+    },
+
+    created () {
+        axios.get(`/api/v1/portfolio/${this.memberInfo.nickname}`)
+        .then(({ data }) => {
+            console.log(data);
+            this.profileData = data;
+        })
+        
+    }
+}
+</script>
