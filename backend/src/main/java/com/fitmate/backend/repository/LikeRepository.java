da2da6e53e1c49053d9506bf646f832c78e0b436
seojiwon0702 <seojiwon335@gmail.com>
2022-02-07 14:03:53 +0900

    FEAT: Implement Like about style

---
+++ backend/src/main/java/com/fitmate/backend/repository/LikeRepository.java
@@ -0,0 +1,17 @@
+package com.fitmate.backend.repository;
+
+import com.fitmate.backend.entity.LikeEntity;
+import com.fitmate.backend.entity.Member;
+import com.fitmate.backend.entity.Style;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface LikeRepository extends JpaRepository<LikeEntity, Long> {
+    Optional<LikeEntity> findByMemberAndStyle(Member member, Style style);
+    int countByStyle(Style style);
+    int countByMemberAndStyle(Member member, Style style);
+    List<LikeEntity> findAllByStyle(Style style);
+    List<LikeEntity> findAllByMember(Member member);
+}
