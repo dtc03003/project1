9429845141ce7869316a16597b6cfc7a573d05aa
박현진 <jinnydiary@naver.com>
2022-02-08 15:42:17 +0900

    feat: 불필요한 것 제거 및 router 추가

--- frontend/fitmate/src/config/index.js
+++ frontend/fitmate/src/config/index.js
@@ -2,6 +2,5 @@ const FITMATE_BASE_URL = "http://localhost:9000";
 
 const KAKAO_APP_KEY = "f08235c075764c7ade497222c61288e1"; //카카오 앱키(js)
 // const KAKAO_APP_KEY = "5a2acf70d166f83e1447c25394f003bb"; //카카오 앱키(js) - 임시
-const KAKAO_ADMIN_KEY = "25960fd628ac40b1e97d76d4ad25b91b"; //카카오 앱키(admin) - 임시
 
-export { FITMATE_BASE_URL, KAKAO_APP_KEY, KAKAO_ADMIN_KEY };
+export { FITMATE_BASE_URL, KAKAO_APP_KEY };
