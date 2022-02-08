2b98a8a514244006a74d8343e5912c3d10cc2eac
박현진 <jinnydiary@naver.com>
2022-02-08 15:36:56 +0900

    feat: 예약&주문 구현 - 각각 토큰 추가 및 관련 api 추가 구현

--- frontend/fitmate/src/api/review.js
+++ frontend/fitmate/src/api/review.js
@@ -4,26 +4,31 @@ const api = apiInstance();
 
 //포트폴리오 존재 여부 확인
 async function findPortfolio(nickname, success, fail) {
+    api.defaults.headers.common["Authorization"] = `Bearer ${localStorage.getItem('accessToken')}`;
     await api.get(`/api/v1/portfolio/${nickname}`).then(success).catch(fail);
 }
 
 //특정 스타일리스트의 리뷰 쓰기
 async function writeReview(review, nickname, success, fail) {
+    api.defaults.headers.common["Authorization"] = `Bearer ${localStorage.getItem('accessToken')}`;
     await api.post(`/api/v1/portfolio/${nickname}/review`, JSON.stringify(review)).then(success).catch(fail);
 }
 
 //해당 스타일리스트의 페이지별 모든 리뷰 불러오기
 async function reviewByPage(info, success, fail) {
+    api.defaults.headers.common["Authorization"] = `Bearer ${localStorage.getItem('accessToken')}`;
     await api.get(`/api/v1/portfolio/${info.nickname}/reviews?page=${info.page}`).then(success).catch(fail);
 }
 
 //해당 스타일리스트의 모든 리뷰 불러오기
 async function reviewAll(nickname, success, fail) {
+    api.defaults.headers.common["Authorization"] = `Bearer ${localStorage.getItem('accessToken')}`;
     await api.get(`/api/v1/portfolio/${nickname}/reviews/all`).then(success).catch(fail);
 }
 
 //해당 스타일리스트의 리뷰 하나씩 불러오기
 async function reviewOne(nickname, id, success, fail) {
+    api.defaults.headers.common["Authorization"] = `Bearer ${localStorage.getItem('accessToken')}`;
     await api.get(`/api/v1/portfolio/${nickname}/review/${id}`).then(success).catch(fail);
 }
 
