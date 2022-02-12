3dbc2c5b6a03a1e6b4b0fff09ef5bd75012c0024
이홍준 <zunza96@naver.com>
2022-02-13 01:07:35 +0900

    FEAT: Token 유효기간 오류 해결

--- backend/src/main/java/com/fitmate/backend/jwt/TokenProvider.java
+++ backend/src/main/java/com/fitmate/backend/jwt/TokenProvider.java
@@ -30,10 +30,8 @@ public class TokenProvider {
 
     private static final String AUTHORITIES_KEY = "auth";
     private static final String BEARER_TYPE = "bearer";
-    @Value("${jwt.token-validity-in-seconds}")
-    private static long ACCESS_TOKEN_EXPIRE_TIME;
-    @Value("${token-refresh-in-seconds}")
-    private static long REFRESH_TOKEN_EXPIRE_TIME;
+    private static final Long ACCESS_TOKEN_EXPIRE_TIME = 86400L;          // 1일
+    private static final Long REFRESH_TOKEN_EXPIRE_TIME = 1000*60*60*24*7L; // 1주일
 
     private final Key key;
 
@@ -118,82 +116,3 @@ public class TokenProvider {
         }
     }
 }
-
-//@Slf4j
-//@Component
-//public class TokenProvider implements InitializingBean {
-//    private static final String AUTHORITIES_KEY = "auth";
-//    private final String secret;
-//    private final long tokenValidityInSeconds;
-//    private Key key;
-//
-//    public TokenProvider(
-//            @Value("${jwt.secret}") String secret,
-//            @Value("${jwt.token-validity-in-seconds}") long tokenValidityInSeconds) {
-//        this.secret = secret;
-//        this.tokenValidityInSeconds = tokenValidityInSeconds * 1000;
-//    }
-//
-//    @Override
-//    public void afterPropertiesSet() throws Exception {
-//        byte[] keyBytes = Decoders.BASE64.decode(secret);
-//        this.key = Keys.hmacShaKeyFor(keyBytes);
-//
-//    }
-//    // create a token
-//    public String createToken(Authentication authentication){
-//        String authorities = authentication.getAuthorities().stream()
-//                .map(GrantedAuthority::getAuthority)
-//                .collect(Collectors.joining(","));
-//        long now = (new Date()).getTime();
-//        Date validity = new Date(now + this.tokenValidityInSeconds);
-//
-//        return Jwts.builder()
-//                .setSubject(authentication.getName())
-//                .claim(AUTHORITIES_KEY, authorities)
-//                .signWith(key, SignatureAlgorithm.HS512)
-//                .setExpiration(validity)
-//                .compact();
-//    }
-//    // get Authentication
-//    public Authentication getAuthentication(String token){
-//        Claims claims = Jwts
-//                .parserBuilder()
-//                .setSigningKey(key)
-//                .build()
-//                .parseClaimsJws(token)
-//                .getBody();
-//        Collection<? extends GrantedAuthority> authorities =
-//                Arrays.stream(claims.get(AUTHORITIES_KEY).toString().split(","))
-//                        .map(SimpleGrantedAuthority::new)
-//                        .collect(Collectors.toList());
-//
-//        User principal = new User(claims.getSubject(), "", authorities);
-//        return new UsernamePasswordAuthenticationToken(principal,token,authorities);
-//    }
-//
-//    // token 유효성 검증
-//    public boolean validateToken(String token){
-//        try {
-//            Jwts.parserBuilder().setSigningKey(key).build().parseClaimsJws(token);
-//            return true;
-//        }catch (io.jsonwebtoken.security.SecurityException | MalformedJwtException e){
-//            log.info("Invalid JWT signature.");
-//        }catch (ExpiredJwtException e){
-//            log.info("Expired JWT signature.");
-//        }catch (UnsupportedJwtException e){
-//            log.info("Unsupported JWT signature.");
-//        }catch (IllegalArgumentException e){
-//            log.info("Invalid JWT token.");
-//        }
-//        return false;
-//    }
-//    private Claims parseClaims(String accessToken){
-//        try{
-//            return Jwts.parserBuilder().setSigningKey(key).build().parseClaimsJws(accessToken).getBody();
-//        }catch (ExpiredJwtException e){
-//            return e.getClaims();
-//        }
-//    }
-//}
-
