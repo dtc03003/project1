faf24f7e6a5d19edb905fab72dc3460cc9fd9e58
이홍준 <zunza96@naver.com>
2022-03-07 04:30:09 +0000

    Update index.js

--- frontend/fitmate/src/config/index.js
+++ frontend/fitmate/src/config/index.js
@@ -1,7 +1,7 @@
-const FITMATE_BASE_URL = "https://192.168.0.5:8443";
+const FITMATE_BASE_URL = [서버주소];
 // const FITMATE_BASE_URL = "https://i6d105.p.ssafy.io:8443";
 
-const KAKAO_APP_KEY = "f08235c075764c7ade497222c61288e1"; //카카오 앱키(js)
+const KAKAO_APP_KEY = [카카오 앱키]; //카카오 앱키(js)
 // const KAKAO_APP_KEY = "5a2acf70d166f83e1447c25394f003bb"; //카카오 앱키(js) - 임시
 
 export { FITMATE_BASE_URL, KAKAO_APP_KEY };
