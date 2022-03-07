c57dcab1f4129e6f112280da3aca11c9935c3f8a
정종혁 <whdgur1068@naver.com>
2022-03-07 17:41:58 +0900

    edit images & delete App key

--- frontend/fitmate/src/config/index.js
+++ frontend/fitmate/src/config/index.js
@@ -2,6 +2,5 @@ const FITMATE_BASE_URL = [서버주소];
 // const FITMATE_BASE_URL = "https://i6d105.p.ssafy.io:8443";
 
 const KAKAO_APP_KEY = [카카오 앱키]; //카카오 앱키(js)
-// const KAKAO_APP_KEY = "5a2acf70d166f83e1447c25394f003bb"; //카카오 앱키(js) - 임시
 
 export { FITMATE_BASE_URL, KAKAO_APP_KEY };
